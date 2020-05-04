16f2991d33e8c9ee7ab4f97c0397217a2c170000
adritnag <tanusreeroy467@gmail.com>
2020-05-04 09:44:55 +0530

    drops are inclded in the code

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+var drops =  []
+var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvexyz";
+
+function setup()
+{   
+    createCanvas(2000,600);
+    frameRate(25);
+    
+    for(var i=0;i<150;i++)
+    {
+        drops.push(new Drop());
+    }
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function draw()
+{
+
+    background(0);
+    for(var i=0;i<drops.length;i++)
+    {
+        drops[i].draw();
+        drops[i].update();
+    }
 }
